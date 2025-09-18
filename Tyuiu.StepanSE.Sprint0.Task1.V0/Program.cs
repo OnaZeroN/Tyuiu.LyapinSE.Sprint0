57024c2f422d6e9e707b3d7d2dd7288103f58ebe
Zeron <dotdot20072607@gmail.com>
2025-09-18 20:19:45 +0500

    Task1

---
+++ Tyuiu.StepanSE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tyuiu.LyapinSE.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Степан";
+            int age = 18;
+
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+
+            Console.ReadKey();
+        }
+    }
+}
