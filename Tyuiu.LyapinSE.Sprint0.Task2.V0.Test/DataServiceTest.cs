aa3531e263d600d3dd4f6f2f7711c4fe94410645
Zeron <dotdot20072607@gmail.com>
2025-09-18 21:04:43 +0500

    Task2

---
+++ Tyuiu.LyapinSE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.LyapinSE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.LyapinSE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Степан";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Степан", res);
+        }
+    }
+}
---
+++ Tyuiu.LyapinSE.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Tyuiu.LyapinSE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.LyapinSE.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Степан"));
+            Console.ReadKey();
+        }
+    }
+}
