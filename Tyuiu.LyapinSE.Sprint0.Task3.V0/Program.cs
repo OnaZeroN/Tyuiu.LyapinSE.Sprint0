e1d5d92796e47ae7c69cecac461cce68768b850f
Zeron <dotdot20072607@gmail.com>
2025-09-18 22:33:38 +0500

    Task3

---
+++ Tyuiu.LyapinSE.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Tyuiu.LyapinSE.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.LyapinSE.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
