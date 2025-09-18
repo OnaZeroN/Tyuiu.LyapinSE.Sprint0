8a39d680216542b8c79ad8c5201d9ba58ef0eaaa
Zeron <dotdot20072607@gmail.com>
2025-09-19 00:13:56 +0500

    Task6

---
+++ Tyuiu.LyapinSE.Sprint0.Task6.V0.Task/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.LyapinSE.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.LyapinSE.Sprint0.Task6.V0.Task
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+
+        [TestMethod]
+        public void CheckSubtractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.AreEqual(-15, res);  
+        }
+
+        [TestMethod]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
